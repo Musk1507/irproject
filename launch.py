4c1a1edc7d7b1ac1baed87e39c95ff30e6fca75b
vishnu-pc <vishnupc1999@gmail.com>
2020-04-09 18:56:33 +0530

    Mark-2.1

--- launch.py
+++ launch.py
@@ -11,13 +11,20 @@ def hello_world():
     <!DOCTYPE html>
     <head>
     <title>ARSG</title>
-    <link rel="stylesheet" href="http://stash.compjour.org/assets/css/foundation.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
     </head>
-    <body style="width: 880px; margin: auto;">  
-        <h1>Automated Essay Summarization and Grading</h1>
-        <p>Ranked sentences: '''+str(ranked_sentence)+''' </p>
-        <p>Summary: '''+str(summary)+''' </p>
-        <p>You have scored: '''+str(round(marks,1))+''' on 10</p>
+    <style>
+    html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif;}
+    h1,h2,h3,h4 {color: Teal;}
+    </style>
+    <body style="width: 880px; margin: auto; background-color: GhostWhite;">
+        <h1><center>Automated Essay Summarization and Grading</center></h1>
+        <p><h4>Ranked sentences:</h4> '''+str(ranked_sentence)+''' </p>
+        <p><h4>Summary:</h4> '''+str(summary)+''' </p>
+        <p><h4>Grade:</h4> You have scored: '''+str(round(marks,1))+'''/10</p>
         <p>These are the spelling mistakes you have made: '''+str(mispelled)+''' </p>
         <p>These are some good vocabulary terms that you have been graded on: '''+str(vocab)+''' </p>
     </body>
